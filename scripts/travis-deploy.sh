4c98a90ca1235a7177c339ef282e4d03403953c2
Sebastian Herzig <Sebastian.J.Herzig@jpl.nasa.gov>
2017-03-02 17:06:52 -0800

    Initial import.

---
+++ scripts/travis-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ev
+
+. $(dirname $0)/travis-decode.sh
+
+# Deploy if TRAVIS_TAG is set.
+# Error if TRAVIS_SECURE_ENV_VARS is false
+
+[ -z "${TRAVIS_TAG}" ] && exit 0;
+[ ! ${TRAVIS_SECURE_ENV_VARS} ] && exit -1;
+
+chmod 600 local.*
+eval `ssh-agent -s`
+ssh-add local.deploy_key
+git config --global push.default simple
+git config --global user.email "nicolas.f.rouquette@jpl.nasa.gov"
+git config --global user.name "Travis CI (on behalf of Nicolas F. Rouquette)"
+
+sbt -jvm-opts travis/jvmopts.compile -Dproject.version=$TRAVIS_TAG publishSigned ghpagesPushSite
+
